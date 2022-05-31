6da1fde28a29a6954041267df95667893a062ca2
kubakerlin <qba@disroot.org>
2022-05-31 01:30:12 +0100

    modified:   main.py

--- main.py
+++ main.py
@@ -1,16 +1,80 @@
+import pandas as pd
+from selenium import webdriver
+from selenium.webdriver.firefox.options import Options
+from bs4 import BeautifulSoup as bs
 
+# Get difficulties. If none exists, default to 0.
+# r1 = orange
+# r2 = yellow
+# r3 = green
+# r4 = gray
+def getDiff(string):
+    color = soup.find("span", class_=string)
 
-profs = [
-        'alchemy',
-        'blacksmithing',
-        'engineering',
-        'enchanting',
-        'jewelcrafting',
-        'leatherworking',
-        'tailoring',
-        'mining',
-        'cooking',
-        'first-aid'
+    if (color is None):
+        return 0
+    else:
+        return color.get_text()
+
+profNames = [
+        'alchemy',          # 0
+        'blacksmithing',    # 1
+        'engineering',      # 2
+        'enchanting',       # 3
+        'jewelcrafting',    # 4
+        'leatherworking',   # 5
+        'tailoring',        # 6
+        'mining',           # 7
+        'cooking',          # 8
+        'first-aid'         # 9
 ]
 
+# Get list of spell IDs for all professions
+profSpellList = []
 
+for prof in profNames:
+    fileName = 'spell-ids/' + prof + '.txt'
+    file = open(fileName, "r")
+
+    # Read each spell ID into list
+    spellList = file.read().splitlines()
+    profSpellList.append(spellList)
+
+# Scrape wowhead for remaining data
+i = 0
+for prof in profSpellList:
+    print("Getting " + profNames[i] + " spells...")
+    profSpells = []
+    for spellID in prof:
+
+        URL = 'https://tbc.wowhead.com/spell=' + spellID
+        print("Scraping " + URL)
+
+        options = Options()
+        options.headless = True
+        browser = webdriver.Firefox(options=options)
+        browser.get(URL)
+        html = browser.page_source
+
+        soup = bs(html, 'lxml')
+
+        # Get data related to the tradeSkill spell
+        spell = []
+        
+        spellName = soup.find_all('h1', class_='heading-size-1')[0].get_text()
+
+        orange = getDiff("r1")
+        yellow = getDiff("r2")
+        green = getDiff("r3")
+        gray = getDiff("r4")
+
+        spell.extend((spellID, spellName, orange, yellow, green, gray))
+        profSpells.append(spell) # Store in main spell list
+
+        browser.quit()
+
+    i += 1
+    spellDF = pd.DataFrame(profSpells, columns = ['spellID', 'spellName', 'orange', 'yellow', 'green', 'gray'])
+    print(spellDF)
+
+    quit()
